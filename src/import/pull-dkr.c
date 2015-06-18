0763187d7b9b84ba9745076f66da86208cd4c202
Pavel Odvody <podvody@redhat.com>
2015-06-18 15:12:19 +0200

    22

--- src/import/pull-dkr.c
+++ src/import/pull-dkr.c
@@ -919,7 +919,8 @@ static void dkr_pull_job_on_finished_v2(PullJob *j) {
                 if (r < 0) {
                         log_error("Digest verification failed!");
                         goto finish;
-                }                     
+                } else 
+                        log_info("Digest verification passed!")
 
                 r = json_parse((const char *)j->payload, &doc);
                 if (r < 0) {
